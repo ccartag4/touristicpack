e857eacc36b4e38c9fc2bcbf29c68f65229535ee
Craig David Cartagena <ccartag4@eafit.edu.co>
2018-02-28 12:56:38 -0500

    bot emulator

--- pi2/app.js
+++ pi2/app.js
@@ -3,8 +3,8 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
 });
 
 // Create chat connector for communicating with the Bot Framework Service
@@ -16,11 +16,7 @@ var connector = new builder.ChatConnector({
 // Listen for messages from users 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
-
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-//var bot = new builder.UniversalBot(connector, function (session) {
-//session.send("You said: %s", session.message.text);
-
-bot.dialog('/', function (session){
-    session.send("how much money you will going to spend");
+bot.dialog('/', function (session) {
+    session.send("Hello world");
 });
+
